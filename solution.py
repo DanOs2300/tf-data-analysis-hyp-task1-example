41685ce5661463d54cb0f54b8acad217ff8ce7dd
DanOs2300 <87863552+DanOs2300@users.noreply.github.com>
2023-04-21 23:24:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels. stats.proportion import proportions_ztest
 
 chat_id = 630197911 # Ваш chat ID, не меняйте название переменной
 
@@ -9,5 +9,5 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
   
-    result = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
+    result = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
     return result[1] < 0.06
